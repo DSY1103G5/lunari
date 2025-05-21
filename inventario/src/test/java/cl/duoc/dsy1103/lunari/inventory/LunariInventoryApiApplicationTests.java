8faaf8c6f0de943e12d3f9dd18c497cfd2a1c45e
omunoz <osca.munozs@duocuc.cl>
2025-05-21 11:11:16 -0400

    feat: se agrega las plantillas de las apis

---
+++ inventario/src/test/java/cl/duoc/dsy1103/lunari/inventory/LunariInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.lunari.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunariInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ usuario/src/main/java/cl/duoc/dsy1103/lunari/user/LunariUserApiApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.lunari.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunariUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunariUserApiApplication.class, args);
+	}
+
+}
