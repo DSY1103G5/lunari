eb74ea5b220f91328047836b5c3106d32d017137
Angelo Millan <ang.millan@duocuc.cl>
2025-05-24 10:14:54 -0400

    se actualiza main class

--- usuario/src/main/java/cl/duoc/lunari/api/user/LunariUserApiApplication.java
+++ usuario/src/main/java/cl/duoc/lunari/api/user/LunariUserApiApplication.java
@@ -1,12 +1,16 @@
-package cl.duoc.dsy1103.lunari.user;
+package cl.duoc.lunari.api.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LunariUserApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(LunariUserApiApplication.class, args);
 	}
 
